43c7ee388cee6a7af5c23c34ea24ef2148d18661
spam80 <45275318+spam80@users.noreply.github.com>
2018-12-07 19:23:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,8 +28,8 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
+    if(message.content === 'Bot ?'){
+        message.channel.send('** By KING **')
     }
 });
 
